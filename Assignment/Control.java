4bdba887dcfb1a8d51a5d548840e69b8e023df7e
kayleighcoffey1 <c14350506@mydit.ie>
2016-04-11 12:18:46 +0100

    Added files via upload

---
+++ Assignment/Control.java
@@ -0,0 +1,18 @@
+package com.test.Assignment;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class Control 
+{
+	public static void main(String[] args) throws FileNotFoundException
+	{
+	
+		//WordCollection word1 = new WordCollection ("fuck");
+		File tweets = new File("abusivetweets.txt");
+	
+	
+	}
+	
+	
+}
